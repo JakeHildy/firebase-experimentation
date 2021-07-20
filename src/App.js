bc0966f3fa9d0ad053d08848ae1cf5d44d797135
Jacob Hildebrandt <jacob.hildebrandt@gmail.com>
2021-07-20 11:45:41 -0700

    Downloading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import firebase from 'firebase';
 
 function App() {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBLCa70vW-59NMffi5cjJveERCJw_cHwzk',
+    authDomain: 'audiotesting-ac501.firebaseapp.com',
+    projectId: 'audiotesting-ac501',
+    storageBucket: 'audiotesting-ac501.appspot.com',
+    messagingSenderId: '408893517975',
+    appId: '1:408893517975:web:2d82db8544a798091439d2',
+  };
+  // Initialize Firebase
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app();
+  }
+
+  const [audioPath, setAudioPath] = useState(null);
+
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  const storage = firebase.storage();
+  const songRef = storage.ref('buffalo-solder.mp3');
+  songRef.getDownloadURL().then((url) => {
+    setAudioPath(url);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='title'>Sample Firebase App</h1>
+      {audioPath && <audio src={audioPath} controls></audio>}
     </div>
   );
 }
